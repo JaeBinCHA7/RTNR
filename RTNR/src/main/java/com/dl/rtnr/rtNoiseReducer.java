4cc6b0b1fe0866fbd157a3d1baa19b8f4162bf0c
JaebinCha <jaebin.cha7@gmail.com>
2022-12-10 20:32:03 +0900

    Real-time noise reducer

---
+++ RTNR/src/main/java/com/dl/rtnr/rtNoiseReducer.java
@@ -0,0 +1,10 @@
+package com.dl.rtnr;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class rtNoiseReducer {
+    public static void showToast(Context context, String s){
+        Toast.makeText(context,s, Toast.LENGTH_LONG).show();
+    }
+}
