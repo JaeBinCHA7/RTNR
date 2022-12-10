05ea4a91ff4166d336794bd9d0b886cf20bba064
JaebinCha <jaebin.cha7@gmail.com>
2022-12-10 21:52:22 +0900

    edit build_grade

--- app/src/main/java/com/dl/real_time_noise_reducer/MainActivity.java
+++ app/src/main/java/com/dl/real_time_noise_reducer/MainActivity.java
@@ -3,11 +3,15 @@ package com.dl.real_time_noise_reducer;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.dl.rtnr.rtNoiseReducer;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rtNoiseReducer.showToast(this, "test");
     }
 }
